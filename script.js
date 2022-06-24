7765f14f293436587e55ea89b10b0e48a2fef57c
beaxxsantos <beasantos.rj@outlook.com>
2022-06-24 08:47:05 -0300

    estrutura de dados

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let nome ="beatriz";
+
+let number = 23;
+
+let ano = 2022;
+
+let sub = ano - number;
+
+let soma = ano + number;
+console.log(soma);
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = false;
+console.log(typeof(quartaFeira));
